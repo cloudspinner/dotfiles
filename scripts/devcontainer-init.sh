efc9e3318d9ab047bc7cb68ed334688971a64ee8
cloudspinner <36537951+cloudspinner@users.noreply.github.com>
2022-05-11 21:07:03 +0000

    Avoid starting tailscale twice.

--- scripts/devcontainer-init.sh
+++ scripts/devcontainer-init.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 sudo /usr/local/share/desktop-init.sh
 sudo /usr/local/share/ssh-init.sh
-sudo tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-sudo tailscale up --authkey=$TAILSCALE_AUTHKEY
+if [ -z "$(sudo netstat -tupln | grep 1055)" ];
+then
+    sudo tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+    sudo tailscale up --authkey=$TAILSCALE_AUTHKEY
+fi
 
 export PLAN9=/usr/local/plan9
 export PATH=$PATH:$PLAN9/bin:/usr/local/go/bin
