f4c83d9bdcf8e69908233388b81b718d6433d82c
cloudspinner <36537951+cloudspinner@users.noreply.github.com>
2022-09-06 17:50:36 +0200

    Fix mosh install

--- install.sh
+++ install.sh
@@ -33,10 +33,10 @@ echo "done"
 
 # Build latest mosh release, to play nice with Neovim colors
 echo "Install mosh...\n"
-sudo apt-get install -y libtinfo-dev libssl-dev protobuf-dev protobuf-compiler
+sudo apt-get install -y libtinfo-dev libssl-dev libprotobuf-dev protobuf-compiler
 cd /home/vscode
 curl -OL https://github.com/mobile-shell/mosh/releases/download/mosh-1.3.2.95rc1/mosh-1.3.2.95rc1.tar.gz
-tar -C /usr/local -xzf mosh-1.3.2.95rc1.tar.gz    
+tar -xzf mosh-1.3.2.95rc1.tar.gz    
 rm mosh-1.3.2.95rc1.tar.gz                        
 cd mosh-1.3.2.95rc1/                                   
 ./configure && make && sudo make install
