f2eb4dd533448395e9f93f38f1e666ac46f17e62
cloudspinner <36537951+cloudspinner@users.noreply.github.com>
2022-11-23 15:09:35 +0100

    Delete Tailscale and SSH installation.
    This is done in container now.

--- install.sh
+++ install.sh
@@ -2,37 +2,12 @@
 dotfiledir="$(dirname "$(realpath "$0")")"
 echo "dotfiles in $dotfiledir"
 
-echo "Installing Go...\n"
-curl -OL https://go.dev/dl/go1.19.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-gobin=/usr/local/go/bin
-echo "done"
-
-echo "Installing Tailscale...\n"
-sudo mkdir -p /usr/local/go/src/tailscale
-cd /usr/local/go/src/tailscale
-sudo git clone https://github.com/tailscale/tailscale.git
-cd tailscale
-$gobin/go mod downloads
-sudo $gobin/go install -mod=readonly ./cmd/tailscaled ./cmd/tailscale
-sudo apt-get update
-sudo apt-get install -y gpg dnsutils
-sudo cp "$dotfiledir"/scripts/tailscaled /etc/init.d # todo: use curl to get tailscaled?
-sudo cp /usr/local/go/bin/tailscaled /usr/sbin/tailscaled
-sudo cp /usr/local/go/bin/tailscale /usr/bin/tailscale
-sudo mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
-echo "done"
-
 <<vnc-comment
 echo "Installing VNC...\n"
 sudo bash "$dotfiledir"/scripts/desktop-lite-debian.sh # todo: use curl to get script?
 echo "done"
 
 vnc-comment
-
-echo "Installing ssh...\n"
-curl -sSL https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/script-library/sshd-debian.sh | sudo bash -s -- 2222 $(whoami) true $SSH_PASSW
-echo "done"
 
 # Build latest mosh release, to play nice with Neovim colors
 echo "Install mosh...\n"
